a47575184e580094f61c3171aad688d88edaa822
stephen <straskal@gmail.com>
2019-01-23 21:39:36 -0500

    more refactoring and adding physFS for io stuff.
    Still have yet to implement

--- src/utilities/Window.cpp
+++ src/utilities/Window.cpp
@@ -5,157 +5,129 @@
 #include "SDL.h"
 
 Window::Window()
-	: Window::Window("Milk", 800, 600, false)
-{	
+        : Window::Window("Milk", 800, 600, false) {
 }
 
 Window::Window(const std::string& title, unsigned int width, unsigned int height, bool fullscreen)
-	: Window::Window(title, width, height, width, height, false)
-{
+        : Window::Window(title, width, height, width, height, false) {
 }
 
-Window::Window(const std::string& title, unsigned int width, unsigned int height, unsigned int virtualWidth, unsigned int virtualHeight, bool fullscreen)
-	: title_(title)
-	, width_(width)
-	, height_(height)
-	, virtualWidth_(virtualWidth)
-	, virtualHeight_(virtualHeight)
-	, isFullscreen_(fullscreen)
-	, sdlWindow_(nullptr)
-	, sdlRenderer_(nullptr)
-{
+Window::Window(const std::string& title, unsigned int width, unsigned int height, unsigned int virtualWidth,
+               unsigned int virtualHeight, bool fullscreen)
+        : title_(title), width_(width), height_(height), virtualWidth_(virtualWidth), virtualHeight_(virtualHeight),
+          isFullscreen_(fullscreen), sdlWindow_(nullptr), sdlRenderer_(nullptr) {
 }
 
-Window::~Window()
-{
-	freeSDLResources();
+Window::~Window() {
+    freeSDLResources();
 }
 
-bool Window::init()
-{
-	SDL_DisplayMode currentDisplayMode;
-	SDL_GetCurrentDisplayMode(0, &currentDisplayMode);
+bool Window::init() {
+    SDL_DisplayMode currentDisplayMode;
+    SDL_GetCurrentDisplayMode(0, &currentDisplayMode);
 
-	int windowXPosition = (currentDisplayMode.w / 2) - (width_ / 2);
-	int windowYPosition = (currentDisplayMode.h / 2) - (height_ / 2);
+    int windowXPosition = (currentDisplayMode.w / 2) - (width_ / 2);
+    int windowYPosition = (currentDisplayMode.h / 2) - (height_ / 2);
 
-	int flags = SDL_WINDOW_SHOWN;
+    int flags = SDL_WINDOW_SHOWN;
 
-	if (isFullscreen_)
-		flags |= SDL_WINDOW_FULLSCREEN;
+    if (isFullscreen_)
+        flags |= SDL_WINDOW_FULLSCREEN;
 
-	sdlWindow_ = SDL_CreateWindow(title_.c_str(), windowXPosition, windowYPosition, width_, height_, (Uint32)flags);
+    sdlWindow_ = SDL_CreateWindow(title_.c_str(), windowXPosition, windowYPosition, width_, height_, (Uint32)flags);
 
-	if (sdlWindow_ == nullptr)
-	{
-		std::cout << "Error creating window: " << SDL_GetError() << std::endl;
-		return false;
-	}
+    if (sdlWindow_ == nullptr) {
+        std::cout << "Error creating window: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-	sdlRenderer_ = SDL_CreateRenderer(sdlWindow_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    sdlRenderer_ = SDL_CreateRenderer(sdlWindow_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	if (sdlRenderer_ == nullptr)
-	{
-		SDL_DestroyWindow(sdlWindow_);
-		std::cout << "Error creating renderer: " << SDL_GetError() << std::endl;
-		return false;
-	}
+    if (sdlRenderer_ == nullptr) {
+        SDL_DestroyWindow(sdlWindow_);
+        std::cout << "Error creating renderer: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
-	SDL_RenderSetLogicalSize(sdlRenderer_, virtualWidth_, virtualHeight_);
-	SDL_SetRenderDrawColor(sdlRenderer_, 0x00, 0x00, 0x00, 0xff); 
-	SDL_SetRenderDrawBlendMode(sdlRenderer_, SDL_BLENDMODE_BLEND);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+    SDL_RenderSetLogicalSize(sdlRenderer_, virtualWidth_, virtualHeight_);
+    SDL_SetRenderDrawColor(sdlRenderer_, 0x00, 0x00, 0x00, 0xff);
+    SDL_SetRenderDrawBlendMode(sdlRenderer_, SDL_BLENDMODE_BLEND);
 
-	return true;
+    return true;
 }
 
-void Window::clear()
-{
-	SDL_SetRenderDrawColor(sdlRenderer_, 0x00, 0x00, 0x00, 0x00);
-	SDL_RenderClear(sdlRenderer_);
+void Window::clear() {
+    SDL_SetRenderDrawColor(sdlRenderer_, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(sdlRenderer_);
 }
 
-void Window::present()
-{
-	SDL_RenderPresent(sdlRenderer_);
+void Window::present() {
+    SDL_RenderPresent(sdlRenderer_);
 }
 
-unsigned int Window::width() const
-{
-	return width_;
+unsigned int Window::width() const {
+    return width_;
 }
 
-unsigned int Window::height() const
-{
-	return height_;
+unsigned int Window::height() const {
+    return height_;
 }
 
-unsigned int Window::virtualWidth() const
-{
-	return virtualWidth_;
+unsigned int Window::virtualWidth() const {
+    return virtualWidth_;
 }
 
-unsigned int Window::virtualHeight() const
-{
-	return virtualHeight_;
+unsigned int Window::virtualHeight() const {
+    return virtualHeight_;
 }
 
-void Window::size(unsigned int width, unsigned int height)
-{
-	width_ = width;
-	height_ = height;
+void Window::size(unsigned int width, unsigned int height) {
+    width_ = width;
+    height_ = height;
 }
 
-void Window::virtualSize(unsigned int virtualWidth, unsigned int virtualHeight)
-{
-	virtualWidth_ = virtualWidth;
-	virtualHeight_ = virtualHeight;
+void Window::virtualSize(unsigned int virtualWidth, unsigned int virtualHeight) {
+    virtualWidth_ = virtualWidth;
+    virtualHeight_ = virtualHeight;
 }
 
-bool Window::fullscreen() const
-{
-	return isFullscreen_;
+bool Window::fullscreen() const {
+    return isFullscreen_;
 }
 
-void Window::toggleFullscreen()
-{
-	isFullscreen_ = !isFullscreen_;
+void Window::toggleFullscreen() {
+    isFullscreen_ = !isFullscreen_;
 
-	if (!isFullscreen_) 
-	{
-		const int WINDOWED = 0;
-		SDL_SetWindowFullscreen(sdlWindow_, WINDOWED);
-		SDL_SetWindowSize(sdlWindow_, width_, height_);
-	}
-	else
-	{
-		int displayIndex = SDL_GetWindowDisplayIndex(sdlWindow_);
+    if (!isFullscreen_) {
+        const int WINDOWED = 0;
+        SDL_SetWindowFullscreen(sdlWindow_, WINDOWED);
+        SDL_SetWindowSize(sdlWindow_, width_, height_);
+    } else {
+        int displayIndex = SDL_GetWindowDisplayIndex(sdlWindow_);
 
-		SDL_Rect displayBounds;
-		SDL_GetDisplayBounds(displayIndex, &displayBounds);
+        SDL_Rect displayBounds;
+        SDL_GetDisplayBounds(displayIndex, &displayBounds);
 
-		nativeWidth_ = (unsigned int)(displayBounds.w);
-		nativeHeight_ = (unsigned int)displayBounds.h;
+        nativeWidth_ = (unsigned int)(displayBounds.w);
+        nativeHeight_ = (unsigned int)displayBounds.h;
 
-		SDL_SetWindowSize(sdlWindow_, nativeWidth_, nativeHeight_);
-		SDL_SetWindowFullscreen(sdlWindow_, SDL_WINDOW_FULLSCREEN);
-	}
+        SDL_SetWindowSize(sdlWindow_, nativeWidth_, nativeHeight_);
+        SDL_SetWindowFullscreen(sdlWindow_, SDL_WINDOW_FULLSCREEN);
+    }
 }
 
-SDL_Window* Window::sdlWindow() const
-{
-	SDL_assert(sdlRenderer_ != nullptr);
-	return sdlWindow_;
+SDL_Window* Window::sdlWindow() const {
+    SDL_assert(sdlRenderer_ != nullptr);
+    return sdlWindow_;
 }
 
-SDL_Renderer* Window::sdlRenderer() const
-{
-	SDL_assert(sdlRenderer_ != nullptr);
-	return sdlRenderer_;
+SDL_Renderer* Window::sdlRenderer() const {
+    SDL_assert(sdlRenderer_ != nullptr);
+    return sdlRenderer_;
 }
 
-void Window::freeSDLResources()
-{
-	SDL_DestroyRenderer(sdlRenderer_);
-	SDL_DestroyWindow(sdlWindow_);
+void Window::freeSDLResources() {
+    SDL_DestroyRenderer(sdlRenderer_);
+    SDL_DestroyWindow(sdlWindow_);
 }
--- src/utilities/Window.h
+++ src/utilities/Window.h
@@ -1,77 +1,80 @@
-#ifndef _WINDOW_
-#define _WINDOW_
+#ifndef _WINDOW_H
+#define _WINDOW_H
 
 #include <string>
 
 struct SDL_Window;
 struct SDL_Renderer;
 
-class Window
-{
+class Window {
 public:
-	Window();
-	Window(const std::string& title, unsigned int width, unsigned int height, bool fullscreen);
-	Window(const std::string& title, unsigned int width, unsigned int height, unsigned int virtualWidth, unsigned int virtualHeight, bool fullscreen);
-	~Window();
+    Window();
 
-	// Initialize the SDL_Window and SDL_Renderer.
-	bool init();
+    Window(const std::string& title, unsigned int width, unsigned int height, bool fullscreen);
 
-	// Clear the renderer to black.
-	void clear();
+    Window(const std::string& title, unsigned int width, unsigned int height, unsigned int virtualWidth,
+           unsigned int virtualHeight, bool fullscreen);
 
-	// Draw the contents of the renderer to the window.
-	void present();
+    ~Window();
 
-	// Get width of window.
-	unsigned int width() const;
+    // Initialize the SDL_Window and SDL_Renderer.
+    bool init();
 
-	// Get height of window.
-	unsigned int height() const;
+    // Clear the renderer to black.
+    void clear();
 
-	// Get vritual width of window.
-	unsigned int virtualWidth() const;
+    // Draw the contents of the renderer to the window.
+    void present();
 
-	// Get virtual height of window.
-	unsigned int virtualHeight() const;
+    // Get width of window.
+    unsigned int width() const;
 
-	// Set size of window.
-	void size(unsigned int width, unsigned int height);
+    // Get height of window.
+    unsigned int height() const;
 
-	// Set the virtual size of window.
-	void virtualSize(unsigned int virtualWidth, unsigned int virtualHeight);
+    // Get vritual width of window.
+    unsigned int virtualWidth() const;
 
-	// Returns true if window is in fullscreen mode.
-	bool fullscreen() const;
+    // Get virtual height of window.
+    unsigned int virtualHeight() const;
 
-	// Toggles fullscreen mode.
-	void toggleFullscreen();
+    // Set size of window.
+    void size(unsigned int width, unsigned int height);
 
-	// Get SDL_Window.
-	SDL_Window* sdlWindow() const;
+    // Set the virtual size of window.
+    void virtualSize(unsigned int virtualWidth, unsigned int virtualHeight);
 
-	// Get SDL_Renderer.
-	SDL_Renderer* sdlRenderer() const;
+    // Returns true if window is in fullscreen mode.
+    bool fullscreen() const;
 
-	// Free all resources.
-	void freeSDLResources();
+    // Toggles fullscreen mode.
+    void toggleFullscreen();
+
+    // Get SDL_Window.
+    SDL_Window* sdlWindow() const;
+
+    // Get SDL_Renderer.
+    SDL_Renderer* sdlRenderer() const;
+
+    // Free all resources.
+    void freeSDLResources();
 
 private:
-	std::string title_;
+    std::string title_;
 
-	unsigned int width_;
-	unsigned int height_;
+    unsigned int width_;
+    unsigned int height_;
 
-	unsigned int nativeWidth_;
-	unsigned int nativeHeight_;
+    unsigned int nativeWidth_;
+    unsigned int nativeHeight_;
 
-	unsigned int virtualWidth_;
-	unsigned int virtualHeight_;
+    unsigned int virtualWidth_;
+    unsigned int virtualHeight_;
 
-	bool isFullscreen_;
+    bool isFullscreen_;
 
-	SDL_Window* sdlWindow_;
-	SDL_Renderer* sdlRenderer_;
+    SDL_Window* sdlWindow_;
+    SDL_Renderer* sdlRenderer_;
 };
 
 #endif
